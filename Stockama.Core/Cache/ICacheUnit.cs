da3ee7a0a06ce587d4dca10ef65c8dde49e5627e
computeneer <computeneer@gmail.com>
2026-02-27 18:28:01 +0300

    nullable fix

--- Stockama.Core/Cache/ICacheUnit.cs
+++ Stockama.Core/Cache/ICacheUnit.cs
@@ -2,8 +2,8 @@ namespace Stockama.Core.Cache;
 
 public interface ICacheUnit
 {
-   T Get<T>(string key);
-   Task<T> GetAsync<T>(string key);
+   T? Get<T>(string key);
+   Task<T?> GetAsync<T>(string key);
    Dictionary<string, T> GetByPattern<T>(string pattern);
    Task<Dictionary<string, T>> GetByPatternAsync<T>(string pattern);
    void Set<T>(string key, T value);
