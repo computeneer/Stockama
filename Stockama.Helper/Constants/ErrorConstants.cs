fa2c5b66ec893a09e10f1ec7db71b6b0b3d948e6
computeneer <computeneer@gmail.com>
2026-02-27 18:13:23 +0300

    Add ErrorConstants class for standardized error messages

---
+++ Stockama.Helper/Constants/ErrorConstants.cs
@@ -0,0 +1,19 @@
+namespace Stockama.Helper.Constants;
+
+public static class ErrorConstants
+{
+   /*   ERROR _ RESOURCES */
+
+   public const string ERROR_INTERNAL_SERVER = "api_error_internalserver_error";
+   public const string ERROR_CONFLICT = "api_error_conflict";
+   public const string ERROR_NOTFOUND = "api_error_notfound";
+   public const string USER_LOGIN_NOTFOUND = "api_auth_usernotfound";
+   public const string USER_NOTFOUND = "api_usernotfound";
+   public const string ERROR_VALIDATION = "api_error_validation";
+   public const string INVALID_TOKEN = "api_error_invalidtoken";
+
+   /*   ERROR _ STRINGS */
+
+   public const string NO_INPUT = "no_input";
+   public const string PREMIUM_REQUIRED = "premium_required";
+}
