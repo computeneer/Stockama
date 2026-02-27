eaab0365cac58eb9ceb5610ed772048e82034555
computeneer <computeneer@gmail.com>
2026-02-27 18:13:35 +0300

    Add QueueConstants class for email queue names

---
+++ Stockama.Helper/Constants/QueueConstants.cs
@@ -0,0 +1,7 @@
+namespace Stockama.Helper.Constants;
+
+public static class QueueConstants
+{
+   public static string ActivationEmailQueueName = "email.activation";
+   public static string ResetPasswordEmailQueueName = "email.resetpassword";
+}
