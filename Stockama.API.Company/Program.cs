734b180e20e0d6cfbdba6523225282d870c3a8e9
computeneer <computeneer@gmail.com>
2026-03-03 20:37:06 +0300

    create new Project

---
+++ Stockama.API.Company/Program.cs
@@ -0,0 +1,32 @@
+using Stockama.Utils.Extensions;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+
+builder.Services.AddEnvironmentVariables();
+
+builder.Services.AddCommonServices();
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.AddCommonServices();
+// app.UseHttpsRedirection();
+
+app.UseCustomAuthenticationMiddleware();
+app.UseAuthenticationMiddleware();
+
+app.MapControllers();
+
+app.Run();
