60043f8c2c26298a50b7dce56f428d15f077678c
computeneer <computeneer@gmail.com>
2026-03-03 20:16:37 +0300

    Add FluentValidation

--- Stockama.Helper/Api/BaseApiController.cs
+++ Stockama.Helper/Api/BaseApiController.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Http;
 using LiteBus.Commands.Abstractions;
 using LiteBus.Queries.Abstractions;
 using LiteBus.Events.Abstractions;
+using Microsoft.Extensions.DependencyInjection;
+using FluentValidation;
 
 namespace Stockama.Helper.Api;
 
@@ -16,18 +18,25 @@ public class BaseApiController : ControllerBase
    protected readonly ICommandMediator _commandMediator;
    protected readonly IQueryMediator _queryMediator;
    protected readonly IEventMediator _eventMediator;
+   private readonly IServiceProvider _serviceProvider;
 
    protected readonly IHttpContextAccessor _httpContextAccessor;
    protected Guid LanguageId;
    protected Guid UserId;
 
-   public BaseApiController(IHttpContextAccessor httpContextAccessor, ICommandMediator commandMediator, IQueryMediator queryMediator, IEventMediator eventMediator)
+   public BaseApiController(
+      IHttpContextAccessor httpContextAccessor,
+      ICommandMediator commandMediator,
+      IQueryMediator queryMediator,
+      IEventMediator eventMediator,
+      IServiceProvider serviceProvider)
    {
       _httpContextAccessor = httpContextAccessor;
       LanguageId = SetLanguageId();
       _commandMediator = commandMediator;
       _queryMediator = queryMediator;
       _eventMediator = eventMediator;
+      _serviceProvider = serviceProvider;
    }
 
    protected Guid SetLanguageId()
@@ -48,6 +57,7 @@ public class BaseApiController : ControllerBase
    protected async Task<object> ForwardCommand<T>(BaseCommandRequest<T> baseRequest)
    {
       baseRequest.LanguageId = LanguageId;
+      await ValidateRequestAsync(baseRequest);
       return await _commandMediator.SendAsync(baseRequest) ?? new object();
    }
 
@@ -55,12 +65,14 @@ public class BaseApiController : ControllerBase
    {
       baseRequest.LanguageId = LanguageId;
       baseRequest.UserId = User.GetUserId();
+      await ValidateRequestAsync(baseRequest);
       return await _commandMediator.SendAsync(baseRequest) ?? new object();
    }
 
    protected async Task<object> ForwardQuery<T>(BaseQueryRequest<T> baseRequest)
    {
       baseRequest.LanguageId = LanguageId;
+      await ValidateRequestAsync(baseRequest);
       return await _queryMediator.QueryAsync(baseRequest) ?? new object();
    }
 
@@ -68,6 +80,29 @@ public class BaseApiController : ControllerBase
    {
       baseRequest.LanguageId = LanguageId;
       baseRequest.UserId = User.GetUserId();
+      await ValidateRequestAsync(baseRequest);
       return await _queryMediator.QueryAsync(baseRequest) ?? new object();
+   }
+
+   private async Task ValidateRequestAsync<T>(T request) where T : class
+   {
+      var validators = _serviceProvider.GetServices<IValidator<T>>().ToList();
+      if (validators.Count <= 0)
+      {
+         return;
+      }
+
+      var context = new ValidationContext<T>(request);
+      var results = await Task.WhenAll(validators.Select(q => q.ValidateAsync(context)));
+      var errors = results
+         .SelectMany(q => q.Errors)
+         .Where(q => q != null)
+         .Select(q => $"{q.PropertyName}: {q.ErrorMessage}")
+         .ToList();
+
+      if (errors.Count > 0)
+      {
+         throw new ValidationException(string.Join(" | ", errors));
+      }
    }
 }
