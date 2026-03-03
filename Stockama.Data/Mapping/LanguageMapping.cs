48689e23408ed4ed228c458b8b6bc21047094683
computeneer <computeneer@gmail.com>
2026-03-03 20:15:41 +0300

    language mapping

--- Stockama.Data/Mapping/LanguageMapping.cs
+++ Stockama.Data/Mapping/LanguageMapping.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using Stockama.Data.Domain;
+using Stockama.Helper.Constants;
 
 namespace Stockama.Data.Mapping;
 
@@ -13,5 +14,15 @@ public class LanguageMapping : BaseTableMapping<Language>
       builder.Property(x => x.Code).IsRequired().HasMaxLength(3);
 
       builder.HasIndex(e => e.Code).IsUnique();
+
+      builder.HasData(new Language
+      {
+         Id = Guid.Parse(ApplicationContants.DefaultLanguageId),
+         Name = "English",
+         Code = ApplicationContants.DefaultLanguageCode,
+         IsDeleted = false,
+         IsActive = true,
+         CreatedAt = new DateTimeOffset(2026, 1, 1, 0, 0, 0, TimeSpan.Zero)
+      });
    }
 }
