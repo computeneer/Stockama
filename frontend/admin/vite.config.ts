0f794e70aa771a36cca386feb948fcb8a36dd73e
computeneer <computeneer@gmail.com>
2026-03-10 20:35:08 +0300

    add alias and server port

--- frontend/admin/vite.config.ts
+++ frontend/admin/vite.config.ts
@@ -1,7 +1,20 @@
+import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5173,
+    strictPort: true,
+  },
+  resolve: {
+    alias: {
+      '@pages': fileURLToPath(new URL('./src/pages', import.meta.url)),
+      '@routes': fileURLToPath(new URL('./src/routes', import.meta.url)),
+      '@layouts': fileURLToPath(new URL('./src/layouts', import.meta.url)),
+      '@components': fileURLToPath(new URL('./src/components', import.meta.url)),
+    },
+  },
 })
