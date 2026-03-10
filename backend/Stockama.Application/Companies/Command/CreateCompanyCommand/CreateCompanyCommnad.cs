c27bab69a1009c509261205cb1abe656d1e0a6bc
computeneer <computeneer@gmail.com>
2026-03-10 20:34:09 +0300

    Get adminusername from model

--- backend/Stockama.Application/Companies/Command/CreateCompanyCommand/CreateCompanyCommnad.cs
+++ backend/Stockama.Application/Companies/Command/CreateCompanyCommand/CreateCompanyCommnad.cs
@@ -10,7 +10,8 @@ public sealed class CreateCompanyCommand : BaseCommandRequest<IBaseBoolResponse>
    public string? LogoUrl { get; set; }
    public string? WebsiteUrl { get; set; }
    public string CompanyCode { get; set; } = string.Empty;
-   public string AdminFirstName { get; set; } = "Tenant";
+   public string AdminUsername { get; set; } = string.Empty;
+   public string AdminFirstName { get; set; } = "Company";
    public string AdminLastName { get; set; } = "Admin";
    public string AdminEmail { get; set; } = string.Empty;
 }
